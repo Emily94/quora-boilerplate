1f258957e7f0287b9d74984e1f364fa8ddd201a2
Emily Sceats <emmysceats@gmail.com>
2018-10-18 14:27:40 +0800

    create table for users, containing name, email and password - all as strings

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative './config/init.rb'
 set :run, true
 
 get '/' do
-  @name = "Bob Smith"
+  @name = "Emily Sceats"
   erb :"home"
+end
+
+get '/' do 
+  @date = DateTime.now
+  erb :'date'
 end
